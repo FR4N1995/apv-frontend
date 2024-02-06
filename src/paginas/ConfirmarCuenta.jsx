c1f8bbbaab296929cb91976ebd0e0421bf669378
FR4N1995 <fran7paco10@gmail.com>
2024-02-05 19:26:37 -0600

    se agrega extencion

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 import {useParams, Link} from 'react-router-dom'
 import axios from 'axios';
-import Alerta from '../components/alerta';
+import Alerta from '../components/Alerta.jsx';
 
 const  ConfirmarCuenta = () => {
   const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
